e8b1b5af84e395baaf4ed0238c27f39924f09f11
04Samir <me@samir.cx>
2024-12-07 13:49:51 +0000

    Implement Menu Dropdown

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -3,6 +3,7 @@ import {
     initialiseSearchEvents,
     updateSearchResults,
 } from './scripts/search.js';
+import { disableDarkMode, enableDarkMode } from './scripts/theme.js';
 
 $(function () {
     $('.modal-backdrop').on('click', function () {
@@ -38,11 +39,55 @@ $(function () {
         }
     });
 
+    initialiseSearchEvents();
+
+    let menuClicked = false;
+
     $(document).on('click', function (event) {
         if (!$(event.target).closest('#menu-button, #menu-dropdown').length) {
             $('#menu-dropdown').addClass('hidden');
+            menuClicked = false;
         }
     });
 
-    initialiseSearchEvents();
+    $('#menu-button').on('click', function () {
+        menuClicked = !menuClicked;
+        $('#menu-dropdown').toggleClass('hidden', !menuClicked);
+    });
+
+    $('#menu-button').on('mouseenter', function () {
+        if (!menuClicked) {
+            $('#menu-dropdown').removeClass('hidden');
+        }
+    });
+
+    $('#menu-button').on('mouseleave', function () {
+        setTimeout(function () {
+            if (!menuClicked && !$('#menu-dropdown').is(':hover')) {
+                $('#menu-dropdown').addClass('hidden');
+            }
+        }, 200);
+    });
+
+    $('#menu-dropdown').on('mouseleave', function () {
+        setTimeout(function () {
+            if (!menuClicked && !$('#menu-button').is(':hover')) {
+                $('#menu-dropdown').addClass('hidden');
+            }
+        }, 200);
+    });
+
+    if (localStorage.getItem('theme') === 'dark') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+
+    $('#theme-mode-toggle').on('click', function () {
+        if (localStorage.getItem('theme') === 'dark') {
+            disableDarkMode();
+        } else {
+            enableDarkMode();
+        }
+    });
 });
