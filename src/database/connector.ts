6bcc126a5e5c1b85a9384c7b9469f3646a283445
04Samir <me@samir.cx>
2024-12-19 21:17:18 +0000

    Test SQL User

--- src/database/connector.ts
+++ src/database/connector.ts
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'appUser',
+    user: 'appUserTT',
     password: 'passWord',
     database: 'trade_ticker',
 });
--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,5 +1,5 @@
-CREATE USER IF NOT EXISTS 'appUser'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'passWord';
-GRANT ALL PRIVILEGES ON *.* TO 'appUser'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'appUserTT'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'passWord';
+GRANT ALL PRIVILEGES ON *.* TO 'appUserTT'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
 DROP DATABASE IF EXISTS trade_ticker;
